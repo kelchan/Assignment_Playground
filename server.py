6046a19d7e66fd06c23bd584c5d831fd63de1229
kelchan <kelvin.chan131@gmail.com>
2022-07-08 14:45:21 -0700

    first commit

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template  # Import Flask to allow us to create our app
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+
+@app.route('/play')          # The "@" decorator associates this route with the function immediately following
+def playground():
+
+    times = 3
+    return render_template("index.html", times = times)
+
+@app.route('/play/<num>/<color>')
+def playground_times( num, color ):
+    times = int(num)
+
+    return render_template( "index.html", times = times, color = color )
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
